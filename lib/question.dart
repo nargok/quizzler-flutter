0b37ee2725500c715628d3595e33d70b9b088c3e
Ryoichi Gokan <nargok@gmail.com>
2020-04-09 18:02:40 +0900

    QuestionType classes

--- lib/question.dart
+++ lib/question.dart
@@ -1,9 +1,37 @@
-class Question {
+class BaseQuestion {
+  /* クイズのすべての形式に共通する雛形 */
   String questionText;
-  bool answer;
 
-  Question(String q, bool a) {
+  BaseQuestion(String q) {
     questionText = q;
-    answer = a;
   }
 }
+
+class TrueFalseQuestion extends BaseQuestion {
+  /* 正誤問題の形式の雛形 */
+  bool answer;
+
+  TrueFalseQuestion(String q, bool a) :
+    answer = a,
+    super(q);
+}
+
+class ThreeChoiceQuestion extends BaseQuestion {
+  /* 3択問題の形式の雛形 */
+  String answer1;
+  String answer2;
+  String answer3;
+  int answerNumber;
+
+  ThreeChoiceQuestion(String q, String a1, String a2, String a3, int aNumber) :
+      answer1 = a1,
+      answer2 = a2,
+      answer3 = a3,
+      answerNumber = aNumber,
+      super(q);
+}
+
+List questionList = [
+  TrueFalseQuestion('犬にたまねぎをたべさせてはいけない', true),
+  ThreeChoiceQuestion('秀吉の幼名は何か？', '日吉丸', '藤吉郎', 'ガルマ・ザビ', 1),
+];
--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -1,32 +1,33 @@
+import 'dart:core';
 import 'package:quizzler/question.dart';
 
 class QuizBrain {
   int _questionNumber = 0;
 
-  List<Question> _questionBank = [
-    Question('Some cats are actually allergic to humans', true),
-    Question('You can lead a cow down stairs but not up stairs.', false),
-    Question('Approximately one quarter of human bones are in the feet.', true),
-    Question('A slug\'s blood is green.', true),
-    Question('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
-    Question('It is illegal to pee in the Ocean in Portugal.', true),
-    Question(
+  List<TrueFalseQuestion> _questionBank = [
+    TrueFalseQuestion('Some cats are actually allergic to humans', true),
+    TrueFalseQuestion('You can lead a cow down stairs but not up stairs.', false),
+    TrueFalseQuestion('Approximately one quarter of human bones are in the feet.', true),
+    TrueFalseQuestion('A slug\'s blood is green.', true),
+    TrueFalseQuestion('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
+    TrueFalseQuestion('It is illegal to pee in the Ocean in Portugal.', true),
+    TrueFalseQuestion(
         'No piece of square dry paper can be folded in half more than 7 times.',
         false),
-    Question(
+    TrueFalseQuestion(
         'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
         true),
-    Question(
+    TrueFalseQuestion(
         'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
         false),
-    Question(
+    TrueFalseQuestion(
         'The total surface area of two human lungs is approximately 70 square metres.',
         true),
-    Question('Google was originally called \"Backrub\".', true),
-    Question(
+    TrueFalseQuestion('Google was originally called \"Backrub\".', true),
+    TrueFalseQuestion(
         'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
         true),
-    Question(
+    TrueFalseQuestion(
         'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
         true),
   ];
